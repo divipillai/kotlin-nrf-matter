aaeb2e0510e2b4fd049953ac2a56b46330689e94
himalia416 <himali.aryal@nordicsemi.no>
2026-01-02 15:22:59 +0100

    Added user preferences data

---
+++ composeApp/src/androidMain/kotlin/no/nordicsemi/nrf/matter/data/UserPreferences.kt
@@ -0,0 +1,96 @@
+package no.nordicsemi.nrf.matter.data
+
+/*
+ * Copyright (c) 2025, Nordic Semiconductor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+
+object UserPreferencesJsonSerializer : Serializer<UserPreferences> {
+
+    override val defaultValue: UserPreferences = UserPreferences()
+
+    override suspend fun readFrom(input: InputStream): UserPreferences {
+        return try {
+            val text = input.readBytes().decodeToString()
+            if (text.isBlank()) defaultValue
+            else Json.decodeFromString(text)
+        } catch (e: Exception) {
+            throw CorruptionException("Cannot read UserPreferences JSON.", e)
+        }
+    }
+
+    override suspend fun writeTo(
+        t: UserPreferences,
+        output: OutputStream
+    ) {
+        val text = Json.encodeToString(t)
+        output.write(text.encodeToByteArray())
+    }
+}
+
+val Context.userPreferencesDataStore: DataStore<UserPreferences> by
+dataStore(fileName = "user_prefs_store.proto", serializer = UserPreferencesJsonSerializer)
+
+/**
+ * User preferences and application settings persisted in DataStore.
+ */
+@Serializable
+data class UserPreferences(
+    /**
+     * Filter for showing / hiding the codelab information on the Home screen.
+     * Using "hide" instead of show so that the default value (false) is the
+     * default behavior we want.
+     */
+    val hideCodelabInfo: Boolean = false,
+
+    /**
+     * Filter for showing / hiding offline devices on the Home screen.
+     * Using "hide" instead of "show" so that the default value (false) is the
+     * default behavior we want (offline devices shown by default).
+     */
+    val hideOfflineDevices: Boolean = false,
+
+    /**
+     * Filter for showing / hiding HalfSheet Notification.
+     *
+     * Default value is false, meaning halfsheet notifications are hidden
+     * by default.
+     */
+    val showHalfsheetNotification: Boolean = false
+)

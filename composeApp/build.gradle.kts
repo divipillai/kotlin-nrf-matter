173ef75cc448e0aed80ae101648c2c0d0b3dfc8a
himalia416 <himali.aryal@nordicsemi.no>
2026-01-27 14:28:20 +0100

    Moved data store dependency to shared dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -34,7 +34,6 @@ kotlin {
             implementation(libs.accompanist.permissions)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(libs.androidx.datastore)
             implementation(libs.play.services.home)
             implementation(libs.matter.android.demo.sdk)
             implementation(libs.androidx.material3)
@@ -58,6 +57,9 @@ kotlin {
             //navigation
             implementation(libs.navigation.compose)
             implementation(libs.kotlinx.serialization.json)
+            // data store
+            implementation(libs.androidx.datastore.preferences)
+            implementation(libs.androidx.datastore)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)

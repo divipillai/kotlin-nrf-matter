45ccdfc8a7ee154c7148cf6ea7c0903537b6e4a1
hiar <aryal.himali@gmail.com>
2026-01-28 16:41:42 +0100

    create nordic theme on common main

---
+++ composeApp/src/commonMain/kotlin/no/nordicsemi/nrf/matter/theme/color.kt
@@ -0,0 +1,105 @@
+package no.nordicsemi.nrf.matter.theme
+
+import androidx.compose.ui.graphics.Color
+
+/* Base brand colors (replace with real hex if needed) *//*
+
+val NordicBlue = Color(0xFF006780) // <-- replace if different
+val NordicSky = Color(0xFF4FB3C8) // <-- replace if different
+
+*/
+/* Material theme colors *//*
+
+val md_theme_primary = NordicBlue
+val md_theme_onPrimary = Color(0xFFFFFFFF)
+
+val md_theme_primaryContainer = NordicSky
+val md_theme_onPrimaryContainer = Color(0xFFFFFFFF)
+
+val md_theme_secondary = NordicBlue
+val md_theme_onSecondary = Color(0xFFFFFFFF)
+
+val md_theme_secondaryContainer = NordicBlue
+val md_theme_onSecondaryContainer = Color(0xFFFFFFFF)
+
+val md_theme_tertiary = Color(0xFF670016)
+val md_theme_onTertiary = Color(0xFFFFB2B6)
+
+val md_theme_tertiaryContainer = Color(0xFFFFB2B6)
+val md_theme_onTertiaryContainer = Color(0xFF670016)
+
+val md_theme_error = Color(0xFFFFB4A9)
+val md_theme_errorContainer = Color(0xFF930006)
+
+val md_theme_onError = Color(0xFF000000)
+val md_theme_onErrorContainer = Color(0xFFFFDAD4)
+
+val md_theme_background = Color(0xFF000000)
+val md_theme_onBackground = Color(0xFFE1E2E5)
+
+val md_theme_surface = Color(0xFF191C1E)
+val md_theme_surfaceContainer = Color(0xFF1B282B)
+val md_theme_surfaceContainerLow = Color(0xFF1B1F22)
+val md_theme_surfaceContainerHigh = Color(0xFF2D3336)
+val md_theme_surfaceContainerHighest = Color(0xFF42484B)
+
+val md_theme_onSurface = Color(0xFFE1E2E5)
+
+val md_theme_surfaceVariant = Color(0xFF40484B)
+val md_theme_onSurfaceVariant = Color(0xFFBFC8CC)
+
+val md_theme_outline = Color(0xFF899296)
+
+val md_theme_inverseOnSurface = Color(0xFF191C1E)
+val md_theme_inverseSurface = Color(0xFFE1E2E5)
+
+val md_theme_primaryInverse = Color(0xFF006780)
+
+// for light theme
+
+
+val light_md_theme_primary = NordicBlue
+val light_md_theme_onPrimary = Color(0xFFFFFFFF)
+
+val light_md_theme_primaryContainer = Color(0xFFB3EBFF)
+val light_md_theme_onPrimaryContainer = Color(0xFF001F29)
+
+val light_md_theme_secondary = NordicLake
+val light_md_theme_onSecondary = Color(0xFFFFFFFF)
+
+val light_md_theme_secondaryContainer = NordicLake
+val light_md_theme_onSecondaryContainer = Color(0xFFFFFFFF)
+
+val light_md_theme_tertiary = NordicBlueslate
+val light_md_theme_onTertiary = Color(0xFFFFFFFF)
+
+val light_md_theme_tertiaryContainer = Color(0xFFD0E4FF)
+val light_md_theme_onTertiaryContainer = Color(0xFF41000A)
+
+val light_md_theme_error = Color(0xFFBA1B1B)
+val light_md_theme_errorContainer = Color(0xFFFFDAD4)
+
+val light_md_theme_onError = Color(0xFFFFFFFF)
+val light_md_theme_onErrorContainer = Color(0xFF410001)
+
+val light_md_theme_background = Color(0xFFFFFFFF)
+val light_md_theme_onBackground = Color(0xFF191C1E)
+
+val light_md_theme_surface = Color(0xFFF5F5F5)
+val light_md_theme_surfaceContainer = Color(0xFFE4EFF2)
+val light_md_theme_surfaceContainerLow = Color(0xFFEAF1F4)
+val light_md_theme_surfaceContainerHigh = Color(0xFFE1E8EB)
+val light_md_theme_surfaceContainerHighest = Color(0xFFDDE4E7)
+
+val light_md_theme_onSurface = Color(0xFF191C1E)
+
+val light_md_theme_surfaceVariant = Color(0xFFDCE4E8)
+val light_md_theme_onSurfaceVariant = Color(0xFF40484B)
+
+val light_md_theme_outline = Color(0xFF70787C)
+
+val light_md_theme_inverseOnSurface = Color(0xFFF0F1F4)
+val light_md_theme_inverseSurface = Color(0xFF2E3133)
+
+val light_md_theme_primaryInverse = Color(0xFF57D5FC)
+*/

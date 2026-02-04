d263d3b5ea8425d53f31eccaa3fbaa512eb592be
hiar <aryal.himali@gmail.com>
2026-02-04 15:15:23 +0100

    App root call in the ios side

--- composeApp/src/iosMain/kotlin/no/nordicsemi/nrf/matter/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/no/nordicsemi/nrf/matter/MainViewController.kt
@@ -1,13 +1,48 @@
 package no.nordicsemi.nrf.matter
 
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import no.nordicsemi.nrf.matter.commission.CommissionHandler
 import platform.UIKit.UIViewController
+
+class IosCommissionHandler(
+    private val onCommission: () -> Unit
+) : CommissionHandler {
+    override fun onCommissioningStarted() {
+        onCommission()
+    }
+}
 
 fun MainViewController(): UIViewController =
     ComposeUIViewController {
         Surface {
-            App()
+            IosAppRoot()
         }
 
     }
+
+@Composable
+fun IosAppRoot() {
+    val commissionHandler = remember {
+        IosCommissionHandler {
+            // Call into Swift / iOS commissioning logic
+            startIosCommissioning()
+        }
+    }
+
+    CompositionLocalProvider(
+        LocalCommissionHandler provides commissionHandler
+    ) {
+        App()
+    }
+}
+
+fun startIosCommissioning() {
+    // Matter commissioning on iOS
+    print("iOS commissioning has started!")
+}
+
+

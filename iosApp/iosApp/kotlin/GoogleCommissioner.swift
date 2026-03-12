414670aef0714f546c10b0a1f8f0154e9e930d8e
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-03-12 14:31:10 +0100

    Add Google Home Api to gitignore.

---
+++ iosApp/iosApp/kotlin/GoogleCommissioner.swift
@@ -0,0 +1,16 @@
+//
+//  GoogleCommissioner.swift
+//  iosApp
+//
+//  Created by Sylwester Zielinski on 12/03/2026.
+//
+
+import ComposeApp
+import GoogleHomeSDK
+
+class GoogleCommissioner : MatterCommissioner {
+    
+    func startIosCommissioning(onError: @escaping () -> Void) async throws -> Device? {
+        return nil
+    }
+}

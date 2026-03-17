3452585b0f113291d0f459e5355830caf24f1240
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-03-17 13:32:20 +0100

    Parsing metadata.

--- iosApp/iosApp/kotlin/google/GoogleHomeCommissioner.swift
+++ iosApp/iosApp/kotlin/google/GoogleHomeCommissioner.swift
@@ -12,6 +12,7 @@ import os.log
 import nrfMatter
 import SharedCode
 import GoogleHomeSDK
+import GoogleHomeTypes
 
 class GoogleHomeCommissioner : MatterCommissioner {
     
@@ -38,17 +39,46 @@ class GoogleHomeCommissioner : MatterCommissioner {
         let request = MatterAddDeviceRequest(topology: topology, shouldScanNetworks: true)
         
         do {
+            try await structure.prepareForMatterCommissioning()
+            
             let storage = MatterStorage()
             storage.storeString(key: SharedConsts.matterEnvStorageKey, value: MatterEnv.google.rawValue)
             
             try await request.perform()
             
-            let nodeID: NSNumber = NodeIdProvider.id // todo
+            guard let commissionedDeviceID = (try await structure.completeMatterCommissioning()).first else {
+                return nil
+            }
             
-            let device = await LocalMatterClusterDiscovery(nodeId: nodeID).discoverClusters()
-            return device
+            print("AAATESTAAA - id: \(commissionedDeviceID)")
+            
+            guard let device = try await home.devices().list().first(where: { $0.id == commissionedDeviceID }) else {
+                return nil
+            }
+            
+            let rootDevice = await device.parts.get(RootNodeDeviceType.self)
+            if let basicInformationTrait = rootDevice?.traits[Matter.BasicInformationTrait.self] {
+                let vendorName = basicInformationTrait.attributes.vendorName
+                let productName = basicInformationTrait.attributes.productName
+                let productID = basicInformationTrait.attributes.productID
+                let vendorID = basicInformationTrait.attributes.vendorID
+                let softwareVersionString = basicInformationTrait.attributes.softwareVersionString
+                
+                return Device(
+                    dateCommissioned: KotlinLong(value: Int64(Date().timeIntervalSince1970 * 1000)),
+                    vendorId: vendorID != nil ? String(vendorID!) : "unknown",
+                    productId: productID != nil ? String(productID!) : "unknown",
+                    deviceType: .lightOnOff,
+                    deviceId: Int64(commissionedDeviceID)!,
+                    name: device.name,
+                    productName: productName,
+                    vendorName: vendorName,
+                    deviceMatterInfo: [] //todo
+                )
+            }
         } catch {
-            logger.info("Failed to set up device with error: \(error.localizedDescription).")
+            let result = structure.markMatterCommissioningFailed(error: error)
+            Logger().error("Failed to complete MatterAddDeviceRequest: \(result.detailedError).")
         }
         return nil
     }

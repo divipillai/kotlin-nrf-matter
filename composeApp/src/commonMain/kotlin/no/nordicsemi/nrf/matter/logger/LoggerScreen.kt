5d18659b61528fef0f68dd552da771c36422bc83
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-05-05 10:50:02 +0200

    Add SearchBar.

--- composeApp/src/commonMain/kotlin/no/nordicsemi/nrf/matter/logger/LoggerScreen.kt
+++ composeApp/src/commonMain/kotlin/no/nordicsemi/nrf/matter/logger/LoggerScreen.kt
@@ -1,45 +1,74 @@
 package no.nordicsemi.nrf.matter.logger
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSearchBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoggerScreen(padding: PaddingValues) {
     val viewModel: LoggerViewModel = koinViewModel()
-    val logs = viewModel.logs.collectAsStateWithLifecycle().value
+    val logs = viewModel.filteredLogs.collectAsStateWithLifecycle().value
+    val searchText = viewModel.filter.collectAsStateWithLifecycle().value
 
-    Box(
+    Column(
         modifier = Modifier
             .padding(padding)
             .fillMaxSize()
     ) {
+        val expanded = rememberSaveable { mutableStateOf(false) }
+
         LazyColumn(
             modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(
+                top = 16.dp,
+                bottom = 16.dp
+            )
         ) {
-            item { Spacer(modifier = Modifier.size(16.dp)) }
+            stickyHeader {
+                SearchBar(
+                    state = rememberSearchBarState(),
+                    inputField = {
+                        SearchBarDefaults.InputField(
+                            query = searchText,
+                            onQueryChange = {
+                                viewModel.setSearch(it)
+                            },
+                            onSearch = {
+                                viewModel.setSearch(it)
+                                expanded.value = false
+                            },
+                            expanded = expanded.value,
+                            onExpandedChange = { expanded.value = it },
+                            placeholder = { Text("Search") }
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                    )
+            }
 
             items(logs.size) {
                 LogItem(logs[it])
             }
-            
-            item { Spacer(modifier = Modifier.size(16.dp)) }
         }
     }
 }
--- composeApp/src/commonMain/kotlin/no/nordicsemi/nrf/matter/logger/LoggerViewModel.kt
+++ composeApp/src/commonMain/kotlin/no/nordicsemi/nrf/matter/logger/LoggerViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 
 class LoggerViewModel(private val logger: PlatformLogger) : ViewModel() {
@@ -13,6 +15,16 @@ class LoggerViewModel(private val logger: PlatformLogger) : ViewModel() {
         SupervisorJob() + Dispatchers.Main
     )
 
-    val logs = logger.getLogs()
+    private val logs = logger.getLogs()
         .stateIn(scope, SharingStarted.Lazily, emptyList())
+
+    val filter = MutableStateFlow("")
+
+    val filteredLogs = logs.combine(filter) { logs, filter ->
+        logs.filter { it.message.lowercase().contains(filter.lowercase()) }
+    }.stateIn(scope, SharingStarted.Lazily, emptyList())
+
+    fun setSearch(value: String) {
+        filter.value = value
+    }
 }

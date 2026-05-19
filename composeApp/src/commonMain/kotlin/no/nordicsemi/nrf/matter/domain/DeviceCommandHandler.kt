34e34e8118a641ae7725682beb136e0d9d48f490
himalia416 <himali.aryal@nordicsemi.no>
2026-05-19 12:40:58 +0200

    binding states as flow

--- composeApp/src/commonMain/kotlin/no/nordicsemi/nrf/matter/domain/DeviceCommandHandler.kt
+++ composeApp/src/commonMain/kotlin/no/nordicsemi/nrf/matter/domain/DeviceCommandHandler.kt
@@ -1,11 +1,18 @@
 package no.nordicsemi.nrf.matter.domain
 
 import io.github.aakira.napier.Napier
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import no.nordicsemi.nrf.matter.binding.BindingUiStates
 import no.nordicsemi.nrf.matter.model.Device
+import no.nordicsemi.nrf.matter.model.DeviceBinding
 import no.nordicsemi.nrf.matter.model.DeviceController
 import no.nordicsemi.nrf.matter.model.DeviceId
 import no.nordicsemi.nrf.matter.model.DeviceType
+import no.nordicsemi.nrf.matter.repository.BindingRepository
 import no.nordicsemi.nrf.matter.repository.DevicesRepository
 import no.nordicsemi.nrf.matter.repository.DevicesStateRepository
 
@@ -43,6 +50,7 @@ class DeviceCommandHandler(
     private val devicesRepository: DevicesRepository,
     private val devicesStateRepository: DevicesStateRepository,
     private val deviceController: DeviceController,
+    private val bindingRepository: BindingRepository,
 ) {
 
     suspend fun execute(
@@ -183,10 +191,12 @@ class DeviceCommandHandler(
             ?.endpoint ?: 0 // TODO: change to exception and handle from UI.
     }
 
-    suspend fun bind(
+    fun bind(
         switchNodeId: DeviceId,
         lightNodeId: DeviceId,
-    ) {
+    ): Flow<BindingUiStates> = flow {
+        emit(BindingUiStates.InProgress)
+
         try {
             deviceController.bind(
                 sourceNodeId = switchNodeId,
@@ -195,11 +205,25 @@ class DeviceCommandHandler(
                 targetEndpoint = 1, // TODO: Add a function call that looks the endpoint of the light where cluster id is configured.
                 clusterId = ON_OFF_CLUSTER_ID, // TODO: Change it to provide the cluster id based on the type of binding.
             )
-        } catch (e: Exception) {
-            Napier.e("Error binding switch to light: ${e.message}", tag = TAG)
-        }
 
-    }
+            val bindingDevice = DeviceBinding(
+                id = "${switchNodeId}_${lightNodeId}_$ON_OFF_CLUSTER_ID",
+                sourceNodeId = switchNodeId,
+                targetNodeId = lightNodeId,
+                sourceEndpoint = 1,
+                targetEndpoint = 1,
+                clusterId = ON_OFF_CLUSTER_ID
+            )
+
+            bindingRepository.save(bindingDevice)
+
+            emit(BindingUiStates.Success(bindingDevice))
+
+        } catch (e: Exception) {
+            Napier.e(e) { "Binding failed: ${e.message}" }
+            emit(BindingUiStates.Error(e.message ?: "Unknown error"))
+        }
+    }.flowOn(Dispatchers.IO)
 
     fun subscribeToButtonChanges(deviceId: DeviceId): Flow<Boolean> {
         return deviceController.subscribeToButtonChanges(deviceId, 1)

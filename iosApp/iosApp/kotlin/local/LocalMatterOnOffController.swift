94a407660d0cb53b0a6aba9b7055a7c6de6886b2
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-06-03 12:50:55 +0200

    Add setting light level.

--- iosApp/iosApp/kotlin/local/LocalMatterOnOffController.swift
+++ iosApp/iosApp/kotlin/local/LocalMatterOnOffController.swift
@@ -23,11 +23,31 @@ class LocalMatterOnOffController : MatterOnOffController {
         let baseDevice = MTRBaseDevice(nodeID: deviceId.nsNumber(), controller: controller)
 
         let cluster = MTRBaseClusterOnOff(device: baseDevice, endpointID: endpoint as NSNumber, queue: DispatchQueue.global())
-        SharedLogger.debug("Cluster created: \(cluster)")
+        SharedLogger.debug("Cluster created: \(String(describing: cluster))")
         if (isOn) {
             try await cluster?.on()
         } else {
             try await cluster?.off()
         }
+    }
+    
+    /**
+     * Set the light level on a remote Matter device.
+     */
+    func setLevel(deviceId: DeviceId, isOn: Bool, endpoint: Int32, level: Int32) async throws {
+        let controller = try LocalControllerProvider(logTag: "LocalControllerProvider").getController()
+        let baseDevice = MTRBaseDevice(nodeID: deviceId.nsNumber(), controller: controller)
+
+        let cluster = MTRBaseClusterLevelControl(
+            device: baseDevice,
+            endpointID: endpoint as NSNumber,
+            queue: DispatchQueue.global()
+        )
+
+        SharedLogger.debug("Cluster created: \(String(describing: cluster))")
+
+        let params = MTRLevelControlClusterMoveToLevelParams()
+        params.level = NSNumber(value: level)
+        try await cluster?.moveToLevel(with: params)
     }
 }

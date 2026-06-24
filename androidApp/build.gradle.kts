b92c1dc93771e3c1e657b37266ae201944883ca1
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-06-24 14:54:19 +0200

    Bump minSdk.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,6 +9,10 @@ android {
     namespace = "no.nordicsemi.nrf.matter.app"
 
     defaultConfig {
+        minSdk = 27
+    }
+
+    defaultConfig {
         applicationId = "no.nordicsemi.nrf.matter"
     }
 }
@@ -26,19 +30,4 @@ dependencies {
     implementation(libs.jetbrains.compose.runtime)
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
-//    implementation(libs.androidx.activity.compose)
-//    implementation(libs.androidx.core.splashscreen)
-//    implementation(libs.koin.android)
-//    implementation(libs.accompanist.permissions)
-//    implementation(libs.androidx.lifecycle.viewmodelCompose)
-//    implementation(libs.androidx.lifecycle.runtimeCompose)
-//    implementation(libs.androidx.material3)
-//    implementation(libs.androidx.material)
-//    implementation(libs.androidx.runtime.livedata)
-//    implementation(libs.room.core)
-//    implementation(libs.room.ktx)
-//    implementation(libs.koin.core)
-//    implementation(libs.koin.androidx.compose)
-//    ksp(libs.room.ksp)
-//    implementation(libs.components.resources)
 }

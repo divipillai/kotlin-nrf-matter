46e85b4e0909e1badf2f210612ca581bea3fc405
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-07-03 12:21:28 +0200

    Readd room.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.nordic.android.kmp.library)
     alias(libs.plugins.nordic.kotlin)
+    alias(libs.plugins.ksp)
 }
 
 group = "no.nordicsemi.nrf.matter.core"
@@ -28,10 +29,19 @@ kotlin {
     }
 
     sourceSets {
+        androidMain.dependencies {
+            implementation(libs.room.runtime)
+            implementation(libs.room.ktx)
+        }
         commonMain.dependencies {
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.datetime)
         }
     }
+}
+
+dependencies {
+    add("kspAndroid", libs.room.compiler)
+    add("kspIosArm64", libs.room.compiler)
 }

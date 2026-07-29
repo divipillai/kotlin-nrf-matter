6c311ad42fce4d9092c4ad09ef48ecc2306a3025
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-07-29 18:22:11 +0200

    Fix suspension of the function.

--- composeApp/src/iosMain/kotlin/no/nordicsemi/nrf/matter/adapters/CancellableContinuationExt.kt
+++ composeApp/src/iosMain/kotlin/no/nordicsemi/nrf/matter/adapters/CancellableContinuationExt.kt
@@ -1,16 +1,20 @@
 package no.nordicsemi.nrf.matter.adapters
 
 import kotlinx.coroutines.CancellableContinuation
+import no.nordicsemi.nrf.matter.logger.NordicLogger
 import platform.Foundation.NSError
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
 fun <T> CancellableContinuation<T>.handleResult(error: NSError?, result: T? = null) {
+    NordicLogger.debug("Handle operation result: $error, $result")
     if (error != null) {
         resumeWithException(IOSException(error))
     } else if (result != null) {
         resume(result)
     } else {
         // Can occur for successful operations without a result.
+        @Suppress("UNCHECKED_CAST")
+        resume(Unit as T)
     }
 }

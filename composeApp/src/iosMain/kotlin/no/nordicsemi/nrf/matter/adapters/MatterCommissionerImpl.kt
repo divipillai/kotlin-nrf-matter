92c21233e1ec779880efb6ddbcfc799fb4810837
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-08-04 13:41:19 +0200

    Fix Swift to Kotlin types mapping.

--- composeApp/src/iosMain/kotlin/no/nordicsemi/nrf/matter/adapters/MatterCommissionerImpl.kt
+++ composeApp/src/iosMain/kotlin/no/nordicsemi/nrf/matter/adapters/MatterCommissionerImpl.kt
@@ -2,13 +2,15 @@
 
 package no.nordicsemi.nrf.matter.adapters
 
+import iosMatter.LocalMatterCommissioner
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import no.nordicsemi.nrf.matter.MatterCommissioner
+import no.nordicsemi.nrf.matter.commission.toCommissioningException
 import no.nordicsemi.nrf.matter.domain.OperationResult
 import no.nordicsemi.nrf.matter.model.Device
 import no.nordicsemi.nrf.matter.model.DeviceId
-import iosMatter.LocalMatterCommissioner
+import kotlin.coroutines.resume
 
 class MatterCommissionerImpl : MatterCommissioner {
 
@@ -17,10 +19,12 @@ class MatterCommissionerImpl : MatterCommissioner {
     override suspend fun startIosCommissioning(deviceId: DeviceId): OperationResult<Device> {
         return suspendCancellableCoroutine { continuation ->
             localMatterCommissioner.startIosCommissioningWithDeviceId(deviceId.toNSNumber()) { device, error ->
-                continuation.handleResult(
-                    error = error,
-                    result = device?.let { OperationResult.Success(it.toDomain()) }
-                )
+                val result = error?.toCommissioningException()
+                    ?.let { OperationResult.Error(it) }
+                    ?: device?.let { OperationResult.Success(it.toDomain()) }
+                    ?: throw IllegalArgumentException("Both error and success are null.")
+
+                continuation.resume(result)
             }
         }
     }
--- composeApp/src/iosMain/kotlin/no/nordicsemi/nrf/matter/commission/CommissioningExceptionMapper.kt
+++ composeApp/src/iosMain/kotlin/no/nordicsemi/nrf/matter/commission/CommissioningExceptionMapper.kt
@@ -2,19 +2,18 @@ package no.nordicsemi.nrf.matter.commission
 
 import no.nordicsemi.nrf.matter.model.DeviceId
 import platform.Foundation.NSError
-import platform.Foundation.NSNumber
 
 fun NSError.toCommissioningException(): CommissioningException? {
-    val deviceIdNumber = userInfo["deviceId"] as? NSNumber ?: return null
-    val stageRawValue = userInfo["stage"] as? Int ?: return null
+    val deviceIdNumber = userInfo["deviceId"] as? Long ?: return null
+    val stageRawValue = userInfo["stage"] as? Long ?: return null
     val displayMsg = userInfo["displayMessage"] as? String ?: return null
-    val fabricId = userInfo["fabricId"] as? Int ?: return null
+    val fabricId = userInfo["fabricId"] as? Long ?: return null
 
     return CommissioningException(
-        deviceId = DeviceId(deviceIdNumber.stringValue),
-        stage = Stage.entries[stageRawValue],
+        deviceId = DeviceId(deviceIdNumber.toString()),
+        stage = Stage.entries[stageRawValue.toInt()],
         errorCode = code.toInt(),
         displayMessage = displayMsg,
-        fabricId = fabricId
+        fabricId = fabricId.toInt()
     )
 }

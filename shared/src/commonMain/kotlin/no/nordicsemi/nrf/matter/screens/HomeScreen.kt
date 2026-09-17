466990bd1a07c77b46d6ad94c4b19e8f1c6f11cc
himalia416 <himali.aryal@nordicsemi.no>
2026-09-17 12:25:32 +0200

    Added source code link

--- shared/src/commonMain/kotlin/no/nordicsemi/nrf/matter/screens/HomeScreen.kt
+++ shared/src/commonMain/kotlin/no/nordicsemi/nrf/matter/screens/HomeScreen.kt
@@ -62,6 +62,9 @@ fun HomeScreen(
                 onAddDeviceClick = onCommissionClick,
                 onMatterUrlClick = {
                     uriHandler.openUri(MATTER_OVERVIEW_URL)
+                },
+                onSourceCodeClick = {
+                    uriHandler.openUri(SOURCE_CODE_URL)
                 }
             )
         } else {
@@ -74,4 +77,7 @@ fun HomeScreen(
 
 private const val MATTER_OVERVIEW_URL =
     "https://nrfconnectdocs.nordicsemi.com/ncs/latest/nrf/protocols/matter/overview/index.html"
+
+private const val SOURCE_CODE_URL =
+    "https://github.com/nordicsemi/kotlin-nrf-matter"
 
